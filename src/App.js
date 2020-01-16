5e27017eb7af2a03eb24b95e72df1b69ec78b604
Nikolas Santis <nikosantis@gmail.com>
2020-01-16 16:05:01 -0300

    Home Page

--- src/App.js
+++ src/App.js
@@ -1,43 +1,22 @@
 import React, { Component } from 'react'
-import { Title } from './components/Title'
-import { SearchForm } from './components/SearchForm'
-import { MoviesList } from './components/MoviesList'
+
+import { Home } from './pages/Home'
 import { Details } from './pages/Details'
 
 import './App.css'
 import 'bulma/css/bulma.css'
 
 class App extends Component {
-  state = { usedSearch: false, results: [] }
-
-  _handleResults = results => {
-    this.setState({ results, usedSearch: true })
-  }
-
-  _renderResults = () => {
-    return this.state.results.length === 0
-      ? <p>Sorry! 😰 Results not found!</p>
-      : <MoviesList movies={this.state.results}/>
-  }
 
   render () {
     const url = new URL(document.location)
-    const hasId = url.searchParams.has('id')
+    const Page = url.searchParams.has('id')
+      ? <Details id={url.searchParams.get('id')}/>
+      : <Home />
 
-    if (hasId) {
-      return <Details id={url.searchParams.get('id')}/>
-    }
     return (
       <div className="App">
-        <Title>Search movies</Title>
-        <div className='SearchForm-wrapper'>
-          <SearchForm onResults={this._handleResults}/>
-        </div>
-        {
-          this.state.usedSearch
-            ? this._renderResults()
-            : <small>Use the form to search a movies</small>
-        }
+        {Page}
       </div>
     )
   }
