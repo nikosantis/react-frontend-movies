df2587cb1eb3e88d908bcb5b27908f36fe03a3f4
Nikolas Santis <nikosantis@gmail.com>
2020-01-16 12:30:19 -0300

    improvements in searchForm

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react'
 import { Title } from './components/Title'
-import { SearchForm } from './components/Search'
+import { SearchForm } from './components/SearchForm'
 import { MoviesList } from './components/MoviesList'
 
 import './App.css'
 import 'bulma/css/bulma.css'
 
 class App extends Component {
-  state = { results: [] }
+  state = { usedSearch: false, results: [] }
 
   _handleResults = results => {
-    this.setState({ results })
+    this.setState({ results, usedSearch: true })
+  }
+
+  _renderResults = () => {
+    return this.state.results.length === 0
+      ? <p>Sorry! 😰 Results not found!</p>
+      : <MoviesList movies={this.state.results}/>
   }
 
   render () {
@@ -20,9 +26,10 @@ class App extends Component {
         <div className='SearchForm-wrapper'>
           <SearchForm onResults={this._handleResults}/>
         </div>
-        {this.state.results.length === 0
-          ? <p>Sin resultados</p>
-          : <MoviesList movies={this.state.results}/>
+        {
+          this.state.usedSearch
+            ? this._renderResults()
+            : <small>Use the form to search a movies</small>
         }
       </div>
     )
